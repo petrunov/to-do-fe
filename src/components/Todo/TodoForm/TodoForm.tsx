1ee87b6c4734f822afbfc262ba20adfc33156dd1
Vesselin Petrunov <petrunov@protonmail.com>
2024-06-13 19:59:45 +0300

    Improved user experience by enhancing FE validation.

--- src/components/Todo/TodoForm/TodoForm.tsx
+++ src/components/Todo/TodoForm/TodoForm.tsx
@@ -1,11 +1,21 @@
 // src/components/Todo/TodoForm/TodoForm.tsx
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { createTodo } from 'services/todoService';
 
 const TodoForm: React.FC = () => {
   const [newTodoTitle, setNewTodoTitle] = useState<string>('');
   const [newTodoDescription, setNewTodoDescription] = useState<string>('');
   const [error, setError] = useState<string | null>(null);
+  const [isButtonDisabled, setIsButtonDisabled] = useState<boolean>(true);
+
+  useEffect(() => {
+    // Enable the button if both fields are filled
+    if (newTodoTitle && newTodoDescription) {
+      setIsButtonDisabled(false);
+    } else {
+      setIsButtonDisabled(true);
+    }
+  }, [newTodoTitle, newTodoDescription]);
 
   const handleCreateTodo = async () => {
     setError(null); // Clear previous errors
@@ -45,15 +55,23 @@ const TodoForm: React.FC = () => {
         value={newTodoTitle}
         onChange={(e) => setNewTodoTitle(e.target.value)}
         placeholder='Title'
+        style={{ borderColor: !newTodoTitle ? 'red' : '' }}
       />
+      {!newTodoTitle && <div style={{ color: 'red' }}>Title is required</div>}
       <input
         type='text'
         value={newTodoDescription}
         onChange={(e) => setNewTodoDescription(e.target.value)}
         placeholder='Description'
+        style={{ borderColor: !newTodoDescription ? 'red' : '' }}
       />
+      {!newTodoDescription && (
+        <div style={{ color: 'red' }}>Description is required</div>
+      )}
       {error && <div style={{ color: 'red' }}>{error}</div>}
-      <button onClick={handleCreateTodo}>Create Todo</button>
+      <button onClick={handleCreateTodo} disabled={isButtonDisabled}>
+        Create Todo
+      </button>
     </div>
   );
 };
