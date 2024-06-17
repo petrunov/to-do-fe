26173390d6d551aa2a200ad1604bf5a53d6c7c24
Vesselin Petrunov <petrunov@protonmail.com>
2024-06-17 03:07:10 +0300

    Better error handling

--- src/pages/todos/index.tsx
+++ src/pages/todos/index.tsx
@@ -15,6 +15,7 @@ const TodosPage: React.FC = () => {
   const [loading, setLoading] = useState<boolean>(false); // Loading state
   const [showModal, setShowModal] = useState(false);
   const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const getTodos = async () => {
@@ -22,8 +23,10 @@ const TodosPage: React.FC = () => {
       try {
         const todosData = await fetchTodos();
         setTodos(todosData);
+        setError(null);
       } catch (error) {
         console.error('Error fetching todos:', error);
+        setError('Failed to load todos. Please try again later.');
       } finally {
         setLoading(false); // Set loading to false after request
       }
@@ -49,6 +52,7 @@ const TodosPage: React.FC = () => {
       setShowModal(false);
     } catch (error) {
       console.error('Error deleting todo:', error);
+      setError('Error deleting todo. Please try again later.');
     } finally {
       setLoading(false); // Set loading to false after request
       setSelectedTodo(null);
@@ -74,6 +78,7 @@ const TodosPage: React.FC = () => {
     try {
       setTodos([...todos, newTodo]);
     } catch (error) {
+      setError('Error creating todo. Please try again later.');
       console.error('Error creating todo:', error);
     } finally {
       setLoading(false); // Set loading to false after request
@@ -93,11 +98,15 @@ const TodosPage: React.FC = () => {
         onCancel={handleCancelDelete}
         onConfirm={handleDeleteTodo}
       />
-      {loading ? (
+      {loading && (
         <div className='flex justify-center items-center h-screen'>
           <ClipLoader color={'#123abc'} loading={loading} size={50} />
         </div>
-      ) : (
+      )}
+      {error && (
+        <p className='text-red-500 font-bold my-5 text-center'>{error}</p>
+      )}
+      {!loading && !error && (
         <TodoList
           todos={todos}
           onUpdateTodo={handleUpdateTodo}
